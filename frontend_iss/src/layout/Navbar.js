b503f992f41a0d438788cb89834ad6802ae44c77
TufaruAlex <35563906+TufaruAlex@users.noreply.github.com>
2023-05-25 23:12:14 +0300

    bucket list stuff

--- frontend_iss/src/layout/Navbar.js
+++ frontend_iss/src/layout/Navbar.js
@@ -1,34 +1,59 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Cookies from "js-cookie";
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 export default function Navbar() {
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container-fluid">
-          <Link className="navbar-brand" href="#" to="/destinations">
-            Bucket List Application
-          </Link>
+    const userId = localStorage.getItem("id");
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" href="#" to="/">
+                        Bucket List Application
+                    </Link>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <Link className="btn btn-outline-light" class="bucket-list" to={"/" + String(userId) + "/bucket-list"}>
+                        View bucket list
+                    </Link>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <Link className="btn btn-outline-light" to="/employee-comparison">
+                        button two
+                    </Link>
 
-          <Link className="btn btn-outline-light" to="/add-employee">
-            button one
-          </Link>
-
-          <Link className="btn btn-outline-light" to="/employee-comparison">
-            button two
-          </Link>
-
-          <Link className="btn btn-outline-light" to="/sort-employee">
-            button three
-          </Link>
-
-          {Cookies.get("zurli")!==undefined &&(
-              <Link className="btn btn-outline-light" to="/logout">
-                Logout
-              </Link>            )}
-        </div>
-      </nav>
-    </div>
-  );
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <Link className="btn btn-outline-light" to="/sort-employee">
+                        button three
+                    </Link>
+                </div>
+            </nav>
+        </div>);
 }
